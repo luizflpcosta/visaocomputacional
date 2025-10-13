36d4120947f15d2805855c53d8fba458a5a745cc
luizflpcosta <luizfelipebcosta007@gmail.com>
2025-10-13 18:55:55 -0300

    projeto finalizado

--- histogram/code.py
+++ histogram/code.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
-import time
 import os
 import matplotlib.pyplot as plt
 
 os.chdir(os.path.dirname(__file__))
+
+# Lê a imagem em tons de cinza
 img = cv2.imread('penguins.png', cv2.IMREAD_GRAYSCALE)
 frame = img.copy()
 
@@ -19,17 +20,24 @@ plt.plot(hist)
 plt.xlim([0, 256])
 plt.show()
 
+# determina o cinza mais frequente
 cinza_mais_frequente = np.argmax(hist)
 print(f'O nível de cinza mais frequente é: {cinza_mais_frequente}')
 
-limiar = 120
-_, img_bin = cv2.threshold(img, limiar, 255, cv2.THRESH_BINARY)
-cv2.imshow('Imagem binária', img_bin)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# define um valor de threshold adequado (de acordo com o histograma)
+threshold_value = 120
+_, img_bin = cv2.threshold(img, threshold_value, 255, cv2.THRESH_BINARY)
 
-img_logica = img_bin // 255
-print('imagem binária lógica (0 e 1):')
+#exibe a imagem original e binária lado a lado
+plt.figure(figsize=(15, 5))
 
+plt.subplot(1, 2, 1)
+plt.title('Imagem Original')
+plt.imshow(img, cmap='gray')
+plt.axis('off')
 
-
+plt.subplot(1, 2, 2)
+plt.title(f'Imagem binária (T={threshold_value})')
+plt.imshow(img_bin, cmap='gray')
+plt.axis('off')
+plt.show()
